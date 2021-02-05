8a8ba65a34c5cfad870c72b6e901acd2becaa46f
Nestor Venegas <nestor.venegas95@gmail.com>
2021-02-05 00:37:51 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -26,5 +26,6 @@ def third_challenge
    }
   }
 
-  
+  :montague = { :lord_montague = {} , :lady_montague = {} , :romeo = {} , :benvolio = {} , :mercutio = {} }
+   :capulet = { :lord_capulet = {} , :lady_capulet = {} , :juliet = {} , :steven = {} , :nurse = {}}
 end
