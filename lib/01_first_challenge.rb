e1c77096c9c9d34f7bbad3f72cb97b11721aba93
Nestor Venegas <nestor.venegas95@gmail.com>
2021-02-05 00:37:27 +0000

    Automatically backed up by Learn

--- lib/01_first_challenge.rb
+++ lib/01_first_challenge.rb
@@ -2,6 +2,4 @@ def first_challenge
    epic_tragedy = {
      :montague = {},
      :capulet = {}}
-   :montague = { :lord_montague = {} , :lady_montague = {} , :romeo = {} , :benvolio = {} , :mercutio = {} }
-   :capulet = { :lord_capulet = {} , :lady_capulet = {} , :juliet = {} , :steven = {} , :nurse = {}}
 end
